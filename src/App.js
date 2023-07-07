7b4477c57434a961e23d381883efb1f02cdf0b4c
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2023-07-07 11:46:54 +0300

    get movies data and print it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { AuthForm } from "./components/AuthForm";
+import { collection, getDocs } from "firebase/firestore";
+import { database } from "./config/firebase";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const moviesCollection = collection(database, "movies");
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const data = await getDocs(moviesCollection);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setMovies(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getMovies();
+  }, []);
+
   return (
     <div className='App'>
       <AuthForm />
+
+      <div style={{ marginTop: "10rem" }}>
+        {movies.map((m) => (
+          <div key={m.id}>
+            <h1 style={{ color: m.receivedAnOscar ? "green" : "red" }}>
+              {m.title}
+            </h1>
+            <p>Date: {m.releaseDate}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,3 +19,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const database = getFirestore(app);
